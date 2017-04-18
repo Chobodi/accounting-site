81a62a34c3a78fe08dc3d7ed711b8800874ffb69
CHOBODI <CHOBODI>
2017-04-18 19:47:44 +0530

    db connection

--- db/user.php
+++ db/user.php
@@ -1,8 +1,8 @@
 <?php 
-	$servername="localhost";
-	$username = "root";
-	$password ="";
-	$dbname = "accounting";
+	$servername="sql12.freesqldatabase.com";
+	$username = " sql12168947";
+	$password =" XlyjDEgPN6";
+	$dbname = "sql12168947";
 
  ?>
  
--- db/user1.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	$servername="sql12.freesqldatabase.com";
-	$username = " sql12168947";
-	$password =" XlyjDEgPN6";
-	$dbname = "sql12168947";
-
- ?>
- 
- 
